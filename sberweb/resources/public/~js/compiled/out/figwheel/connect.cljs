7a3fe9207e9e868ff367b36806c1435499dd186b
Alexey Zorchenkov <alexey@t5p.root>
2017-02-06 10:32:53 +0300

    deleting compiled javascript files

--- sberweb/resources/public/~js/compiled/out/figwheel/connect.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect (:require [figwheel.client] [figwheel.client.utils] [sberweb.login]))
-(figwheel.client/start {:build-id "app", :websocket-url "ws://localhost:3449/figwheel-ws"})
-
--- sberweb/resources/public/~js/compiled/out/sberweb/common.cljc
+++
@@ -1,7 +0,0 @@
-(ns sberweb.common)
-
-(defn shared-fn
-  "A function that is shared between clj and cljs"
-  []
-  (println "cljc!"))
-
